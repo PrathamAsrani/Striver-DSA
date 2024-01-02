6e783281c55913ac73cea871eba86a6d1a73b09c
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-01-02 20:30:12 +0530

    Practise

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main(){
     vector<int> v = {1, 2, 3, 4, 5, 6};
-    cout << *(v.begin());
-    v.erase(v.begin());
+    cout << *(v.begin()) << "\n";
+    v.erase(v.begin()+1);
+    for(auto i : v){
+        cout << i << ' ';
+    }
     cout << "\n" << *(v.begin());
     cout << "\nINdex of 5: " << (find(v.begin(), v.end(), 5) - v.begin());
     v.erase(v.begin() + (find(v.begin(), v.end(), 5) - v.begin()));
