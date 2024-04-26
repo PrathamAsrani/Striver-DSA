e88031540107f76d14c2ec7e68e152c025823e06
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-04-26 20:55:32 +0530

    nCr

---
+++ elitmus.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int helper(vector<int> &arr){
+    vector<int> a = arr;
+    sort(a.begin(), a.end());
+    int ans = 0, n = arr.size();
+    for(int i = 1; i < n; i++){
+        if(arr[i] < arr[i-1]) {
+            ans++;
+            continue;
+        }
+        int idx = lower_bound(a.begin(), a.end(), arr[i-1]) - a.begin();
+        if(arr[idx+1] != arr[i]){
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {17, 13, 16, 19, 25, 23};
+    cout << helper(arr) << "\n";
+}
